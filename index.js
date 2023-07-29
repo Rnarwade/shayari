1384c5a97820d2f201b852f8f875d29d5188340c
ritesh <narwaderitesh@gmail.com>
2023-07-29 14:26:51 +0530

    first commit for testing

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.get('/',(req,res)=>{
+res.send("welcome to my website")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("database connect")
+})
